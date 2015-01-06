6fed094f76884d3d2bf7ba567d1f6be3205c1068
Lianna Katrdzhyan <lkatrdzhyan@gmail.com>
2015-01-06 11:27:46 -0800

    Lecture examples

--- index.php
+++ index.php
@@ -13,7 +13,25 @@
 	}
 
 	$person1 = new Person();
-	$person1->firstName = "Lianna";
+	$person1->firstname = "Lianna";
 	$person1->lastname = "K";
 
 	print "The person's name is {$person1->getName()}.";
+
+	class Cat {
+		public $firstname = "default name";
+		public $lastname = "default last name";
+		public $gender = "male";
+		public $price = 0;
+
+		function getName() {
+			return "{$this->firstname}";
+			"{$this->lastname}";
+		}
+	}
+
+	$cat1 = new Cat();
+	$cat1->firstname = "Benji";
+	$cat1->lastname = "K";
+
+	print "The cat's name is {$cat1->getName()}.";
