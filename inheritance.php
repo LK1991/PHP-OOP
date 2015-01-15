d1bd38ab90e2d03234f2aef6dff13e33f9b81be3
Lianna Katrdzhyan <lkatrdzhyan@gmail.com>
2015-01-14 21:47:08 -0800

    Lecture 5

--- inheritance.php
+++ inheritance.php
@@ -6,17 +6,14 @@ class Animal {
 	public $scientificName;
 	public $gender;
 	public $color;
-	public $moo;
-	public $howl;
 
-	function __construct($firstName, $lastName, $scientificName, $gender, $color, $moo, $howl) {
+
+	function __construct($firstName, $lastName, $scientificName, $gender, $color) {
 		$this->firstName = $firstName;
 		$this->lastName = $lastName;
 		$this->scientificName = $scientificName;
 		$this->gender = $gender;
 		$this->color = $color;
-		$this->moo = $moo;
-		$this->howl = $howl;
 	}
 
 	function getName() {
@@ -26,12 +23,20 @@ class Animal {
 }
 
 class Cow extends Animal {
+	function __construct($firstName, $lastName, $scientificName, $gender, $color, $moo) {
+		parent::__construct($firstName, $lastName, $scientificName, $gender, $color);
+		$this->moo = $moo;
+	}
 	function greet() {
 		return $this->moo;
 	}
 }
 
 class Wolf extends Animal {
+	function __construct($firstName, $lastName, $scientificName, $gender, $color, $howl) {
+		parent::__construct($firstName, $lastName, $scientificName, $gender, $color);
+		$this->howl = $howl;
+	}
 	function greet() {
 		return $this->howl;
 	}
@@ -48,17 +53,13 @@ class Person {
 	public $gender;
 	public $singer;
 	public $guitarist;
-	public $microphone;
-	public $guitar;
 
-	function __construct($firstName, $lastName, $gender, $singer, $guitarist, $microphone, $guitar) {
+	function __construct($firstName, $lastName, $gender, $singer, $guitarist) {
 		$this->firstName = $firstName;
 		$this->lastName = $lastName;
 		$this->gender = $gender;
 		$this->singer = $singer;
 		$this->guitarist = $guitarist;
-		$this->microphone = $microphone;
-		$this->guitar = $guitar;
 	}
 
 	function getName() {
@@ -68,12 +69,20 @@ class Person {
 }
 
 class Singer extends Person {
+	function __construct($firstName, $lastName, $gender, $singer, $guitarist, $microphone) {
+		parent::__construct($firstName, $lastName, $gender, $singer, $guitarist);
+		$this->microphone = $microphone;
+	}
 	function greet() {
 		return $this->microphone;
 	}
 }
 
 class Guitarist extends Person {
+	function __construct($firstName, $lastName, $gender, $singer, $guitarist, $guitar) {
+		parent::__construct($firstName, $lastName, $gender, $singer, $guitarist);
+		$this->guitar = $guitar;
+	}
 	function greet() {
 		return $this->guitar;
 	}
@@ -89,31 +98,37 @@ class SuperHero {
 	public $marvelHeroName2;
 	public $movie1;
 	public $movie2;
-	public $power1;
-	public $power2;
 
-	function __construct($marvelHeroName1, $marvelHeroName2, $movie1, $movie2, $power1, $power2) {
+	function __construct($marvelHeroName1, $marvelHeroName2, $movie1, $movie2) {
 		$this->marvelHeroName1 = $marvelHeroName1;
 		$this->marvelHeroName2 = $marvelHeroName2;
 		$this->movie1 = $movie1;
 		$this->movie2 = $movie2;
-		$this->power1 = $power1;
-		$this->power2 = $power2;
 	}
 
 	function getName() {
-		return "Superhero 1 is the " . $this->marvelHeroName1 . ", he's from the movie" . $this->movie1 . "and his power is having " . $this->power1 . "in his hands and body" .
-		" and Superhero 2 is " . $this->marvelHeroName2 . ", he's from the movie" . $this->movie2 . "and his power is using a " . $this->power2 . " to fight his enemies";
+		return "Superhero 1 is the " . $this->marvelHeroName1 . ", he's from the movie" . $this->movie1 . "and his power is having " . 
+		$this->power1 . "in his hands and body" .
+		" and Superhero 2 is " . $this->marvelHeroName2 . ", he's from the movie" . $this->movie2 . "and his power is using a " . 
+		$this->power2 . " to fight his enemies";
 	}
 }
 
 class Wolverine extends SuperHero {
+	function __construct($marvelHeroName1, $marvelHeroName2, $movie1, $movie2, $power1) {
+		parent::__construct($marvelHeroName1, $marvelHeroName2, $movie1, $movie2);
+		$this->power1 = $power1;
+	}
 	function greet() {
 		return $this->power1;
 	}
 }
 
 class Thor extends SuperHero {
+	function __construct($marvelHeroName1, $marvelHeroName2, $movie1, $movie2, $power2) {
+		parent::__construct($marvelHeroName1, $marvelHeroName2, $movie1, $movie2);
+		$this->power2 = $power2;
+	}
 	function greet() {
 		return $this->power2;
 	}
